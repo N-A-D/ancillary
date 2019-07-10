8209e5e260f20e1cfc0f300aa28c9ae190a537c7
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-07-09 23:26:33 -0400

    Added traits for key comparison transparency

--- include/ancillary/detail/flat_tree.hpp
+++ include/ancillary/detail/flat_tree.hpp
@@ -1,1 +1,29 @@
 #pragma once
+
+#include <vector>
+#include <cassert>
+#include <iterator>
+
+namespace ancillary {
+	namespace detail {
+
+		template <
+			class Compare,
+			class Key,
+			class = void
+		> struct is_transparent
+			: std::false_type {};
+		
+		template <
+			class Compare,
+			class Key
+		> struct is_transparent<Compare, Key, std::void_t<typename Compare::is_transparent>>
+			: std::true_type {};
+
+		template <
+			class Compare,
+			class Key
+		> constexpr bool is_transparent_v = is_transparent<Compare, Key>::value;
+
+	}
+}
