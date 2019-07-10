60bc55f722ebd8451f34a2a86a22c3c43808f1d9
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-07-10 00:03:59 -0400

    Added key extraction policies

---
+++ include/ancillary/detail/extract_key.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <utility>
+
+namespace ancillary {
+	namespace detail {
+
+		template <
+			class T
+		> struct identity {
+			using type = T;
+			const T& operator()(const T& t) const {
+				return t;
+			}
+		};
+
+		template <
+			class Pair
+		> struct select1st;
+
+		template <
+			class F,
+			class S,
+		> struct select1st<std::pair<F, S>>	{
+			using type = F;
+			const F& operator()(const std::pair<F, S>& p) const {
+				return p.first;
+			}
+		};
+
+	}
+}
