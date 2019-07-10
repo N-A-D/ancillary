1c53b35434f2120ba39275c0dc1dc7d245e14664
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-07-09 23:02:31 -0400

    Add project files.

---
+++ ancillary/ancillary.cpp
@@ -0,0 +1,12 @@
+﻿// ancillary.cpp : Defines the entry point for the application.
+//
+
+#include "ancillary.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
