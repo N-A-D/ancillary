00e956604d4a36484350c6a3d10f0fc0f79470e1
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-07-11 11:28:54 -0400

    Moved some test components to their own headers

---
+++ test/constants.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cstddef>
+
+const std::size_t N = 50;
---
+++ test/employee.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+class Employee { // For testing transparent comparison functors
+public:
+	Employee()
+		: id(s_id++), name("Employee #" + std::to_string(id)) {}
+
+	int get_id() const noexcept { return id; }
+	std::string get_name() const noexcept { return name; }
+private:
+	inline static int s_id = 0;
+	int id;
+	std::string name;
+};
+
+struct EmployeeCompare {
+
+	using is_transparent = std::true_type;
+
+	bool operator()(const Employee& lhs, const Employee& rhs) const noexcept {
+		return lhs.get_id() < rhs.get_id();
+	}
+	bool operator()(const Employee& lhs, int id) const noexcept {
+		return lhs.get_id() < id;
+	}
+	bool operator()(int id, const Employee& rhs) const noexcept {
+		return id < rhs.get_id();
+	}
+};
